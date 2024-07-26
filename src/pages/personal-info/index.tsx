d33067aad9131b7f04fbdb9fb20d4c1f03f0aebe
artFetisov <fetishfestoff@mail.ru>
2024-07-26 19:34:38 +0300

    IN-115 init: general profile

--- src/pages/personal-info/index.tsx
+++ src/pages/personal-info/index.tsx
@@ -1,13 +1,8 @@
+import { Profile } from '@/src/entities/profile'
 import { Inter } from 'next/font/google'
-import Link from 'next/link'
 
 const inter = Inter({ subsets: ['latin'] })
 
 export default function PersonalInfo() {
-  return (
-    <>
-      <h2>PersonalInfo</h2>
-      <Link href="/">back</Link>
-    </>
-  )
+  return <Profile />
 }

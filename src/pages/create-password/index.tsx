eddc8c3713c0e465a69981239e4e89324a2683d1
Dima <dmitrijs.visnakovs@inbox.lv>
2024-07-27 22:36:19 +0300

    add validation

--- src/pages/create-password/index.tsx
+++ src/pages/create-password/index.tsx
@@ -1,8 +1,5 @@
 import { CreateNewPassword } from '@/src/entities/create-password/ui/CreateNewPassword'
-import { Inter } from 'next/font/google'
 import Link from 'next/link'
-
-const inter = Inter({ subsets: ['latin'] })
 
 export default function CreatePassword() {
   return (

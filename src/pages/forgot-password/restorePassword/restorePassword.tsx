84069bf21515e651bf8e4ae10e9e2992e02d4b72
natte <natte.noshimo@gmail.com>
2024-07-29 15:13:38 +0300

    IN-118 feat: ForgotPassword WIP

--- src/pages/forgot-password/restorePassword/restorePassword.tsx
+++ src/pages/forgot-password/restorePassword/restorePassword.tsx
@@ -1,15 +1,12 @@
 import * as React from 'react'
-import { useForm } from 'react-hook-form'
+import { useState } from 'react'
+import { Controller, useForm } from 'react-hook-form'
 
 import { Button, Card, Input, Typography } from '@bitovyevolki/ui-kit-int'
 import { zodResolver } from '@hookform/resolvers/zod'
-import clsx from 'clsx'
 import { z } from 'zod'
 
 import s from './restorePassword.module.scss'
-
-// const emailRegex =
-//   /^([\w-]+(?:\.[\w-]+)*)@((?:[\w-]+\.)*\w[\w-]{0,66})\.([a-z]{2,6}(?:\.[a-z]{2})?)$/
 
 const schema = z.object({
   email: z.string().email(),
@@ -17,22 +14,24 @@ const schema = z.object({
 
 type Fields = z.infer<typeof schema>
 
-type Props = {}
-
-export const RestorePassword = (props: Props) => {
+export const RestorePassword = () => {
   const {
+    control,
     formState: { errors },
     handleSubmit,
-    register,
   } = useForm<Fields>({ resolver: zodResolver(schema) })
+
+  const [isLinkSent, setLinkSent] = useState(false)
+
   const onSubmit = handleSubmit(data => {
     console.log(data)
+    setLinkSent(!isLinkSent)
   })
 
   return (
     <div className={s.wrapper}>
       <Card as={'div'} className={s.card}>
-        <Typography as={'h1'} className={s.formTitle} variant={'h2'}>
+        <Typography as={'h1'} className={s.accentColor} variant={'h2'}>
           Forgot password
         </Typography>
         <form className={s.form} onSubmit={onSubmit}>
@@ -40,23 +39,30 @@ export const RestorePassword = (props: Props) => {
             <label className={s.caption} htmlFor={'email'}>
               Email
             </label>
-            <Input
-              {...register('email')}
-              onChange={() => {}}
-              placeholder={'Epam@epam.com'}
-              value={'email'}
-              variant={'base'}
+            <Controller
+              control={control}
+              name={'email'}
+              render={({ field }) => (
+                <Input
+                  {...field}
+                  error={errors.email?.message}
+                  id={'email'}
+                  onChange={field.onChange}
+                  placeholder={'Epam@epam.com'}
+                  variant={'base'}
+                />
+              )}
             />
           </div>
-          {errors.email?.message && (
-            <Typography as={'p'} className={s.red} variant={'caption'}>
-              {errors.email?.message}
-            </Typography>
-          )}
-          <Typography as={'p'} className={s.caption} variant={'caption'}>
+          <Typography as={'p'} className={s.secondaryColor} variant={'caption'}>
             Enter your email address and we will send you further instructions.
           </Typography>
-          <Button fullWidth variant={'primary'}>
+          {isLinkSent && (
+            <Typography as={'p'} className={s.accentColor} variant={'caption'}>
+              The link has been sent by email. If you don’t receive an email send link again
+            </Typography>
+          )}
+          <Button fullWidth type={'submit'} variant={'primary'}>
             Send link
           </Button>
           <Button as={'a'} fullWidth href={'/signin'} variant={'ghost'}>

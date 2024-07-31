b6942bbcbd11ab7b1e870626b51938d157b4227c
Dima <dmitrijs.visnakovs@inbox.lv>
2024-07-31 20:40:21 +0300

    changes

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,6 +1,6 @@
 // import type { AppProps } from 'next/app'
 
-import { AppProps } from '@/node_modules/next/app'
+import { AppProps } from 'next/app'
 import '../styles/globals.scss'
 
 export default function App({ Component, pageProps }: AppProps) {

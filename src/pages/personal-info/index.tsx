f5b0a5cecfec72e8358a7ed4226cb0493522fcf3
Dima <dmitrijs.visnakovs@inbox.lv>
2024-08-01 07:42:00 +0300

    changes

--- src/pages/personal-info/index.tsx
+++ src/pages/personal-info/index.tsx
@@ -1,8 +1,19 @@
 // // import { Profile } from '@/src/entities/profile'
-import { Inter } from 'next/font/google'
+// import { Inter } from 'next/font/google'
+
+import Link from 'next/link'
 
 // const inter = Inter({ subsets: ['latin'] })
 
 // export default function PersonalInfo() {
 //   return <Profile />
 // }
+
+export default function PersonalInfo() {
+  return (
+    <>
+      <Link href="/">back</Link>
+      {/* <CreateNewPassword /> */}
+    </>
+  )
+}

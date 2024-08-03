dbd587780ebe00d73fca31a8ca7072fed6a5f3de
VladimirS <v.a.samarin@yandex.ru>
2024-08-03 09:38:39 +0300

    add paddingBottom

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -2,14 +2,12 @@ import Link from 'next/link'
 
 export default function Home() {
   return (
-    <>
-      <div>
-        <nav className={'navigation'}>
-          <Link href={'/forgot-password'}>Forgot password</Link>
-          <Link href={'/signin'}>Sign In</Link>
-          <Link href={'/signup'}>Sign Up</Link>
-        </nav>
-      </div>
-    </>
+    <div style={{ paddingBottom: '24px' }}>
+      <nav className={'navigation'}>
+        <Link href={'/forgot-password'}>Forgot password</Link>
+        <Link href={'/signin'}>Sign In</Link>
+        <Link href={'/signup'}>Sign Up</Link>
+      </nav>
+    </div>
   )
 }

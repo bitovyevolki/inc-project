b0f8023082a0b558cc0351d03ed6ee38775baf9d
NatteNata <153759340+NatteNata@users.noreply.github.com>
2024-08-03 21:46:42 +0300

    Revert "create and forgot password"

--- src/pages/forgot-password/index.tsx
+++ src/pages/forgot-password/index.tsx
@@ -1,19 +1,13 @@
-import { RestorePassword } from '@/src/features/auth/ForgotPassword/ui/RestorePassword'
-import { Header } from '@bitovyevolki/ui-kit-int'
-import { Inter } from 'next/font/google'
 import Link from 'next/link'
 // import { Inter } from 'next/font/google'
 
 // const inter = Inter({ subsets: ['latin'] })
 
-const inter = Inter({ subsets: ['latin'] })
-
 export default function ForgotPassword() {
   return (
-    <>
-      <Header isAuth onLanguageChange={() => {}} title={'Inctagram'} />
+    <div>
+      <h2>forgotpassword</h2>
       <Link href={'/'}>back</Link>
-      <RestorePassword />
-    </>
+    </div>
   )
 }

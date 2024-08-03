1e720e344e1954f754624de0b478cfdc500136d8
Dima <dmitrijs.visnakovs@inbox.lv>
2024-08-03 19:58:59 +0300

    create password

--- src/features/auth/create-password/ui/CreateNewPassword.tsx
+++ src/features/auth/create-password/ui/CreateNewPassword.tsx
@@ -1,88 +1,101 @@
-import React from 'react'
-// import { Controller, useForm } from 'react-hook-form'
+import React, { useEffect, useState } from 'react'
+import { Controller, useForm } from 'react-hook-form'
+import { Button, Card, FormInput, Typography } from '@bitovyevolki/ui-kit-int'
+import { zodResolver } from '@hookform/resolvers/zod'
+import * as z from 'zod'
+import s from './CreateNewPassword.module.scss'
 
-// import { Button, Card, Input, Typography } from '@bitovyevolki/ui-kit-int'
-// import { zodResolver } from '@hookform/resolvers/zod'
-// import * as z from 'zod'
+const schema = z
+  .object({
+    confirmPassword: z
+      .string()
+      .min(6, 'Password must be at least 6 characters long')
+      .max(20, 'Password must be at most 20 characters long'),
+    newPassword: z
+      .string()
+      .min(6, 'Password must be at least 6 characters long')
+      .max(20, 'Password must be at most 20 characters long'),
+  })
+  .refine(data => data.newPassword === data.confirmPassword, {
+    message: 'The passwords must match',
+    path: ['confirmPassword'],
+  })
 
-// import s from './CreateNewPassword.module.scss'
+export const CreateNewPassword = () => {
+  const [isClient, setIsClient] = useState(false)
 
-// const schema = z
-//   .object({
-//     confirmPassword: z
-//       .string()
-//       .min(6, 'Password must be at least 6 characters long')
-//       .max(20, 'Password must be at most 20 characters long'),
-//     newPassword: z
-//       .string()
-//       .min(6, 'Password must be at least 6 characters long')
-//       .max(20, 'Password must be at most 20 characters long'),
-//   })
-//   .refine(data => data.newPassword === data.confirmPassword, {
-//     message: 'The passwords must match',
-//     path: ['confirmPassword'],
-//   })
+  useEffect(() => {
+    setIsClient(true)
+  }, [])
 
-// export const CreateNewPassword = () => {
-//   const {
-//     control,
-//     formState: { errors },
-//     handleSubmit,
-//   } = useForm({
-//     resolver: zodResolver(schema),
-//   })
+  const {
+    control,
+    formState: { errors },
+    handleSubmit,
+  } = useForm({
+    resolver: zodResolver(schema),
+  })
 
-//   interface FormData {
-//     confirmPassword: number | string
-//     newPassword: number | string
-//   }
+  interface FormData {
+    confirmPassword: string
+    newPassword: string
+  }
 
-//   const onSubmit = (data: FormData) => {
-//     console.log(data)
-//   }
+  const onSubmit = (data: FormData) => {
+    console.log(data)
+  }
 
-//   return (
-//     <div className={s.wrapper}>
-//       <Card className={s.card}>
-//         <h3 className={s.text}>Create New Password</h3>
-//         <form className={s.form} onSubmit={handleSubmit(onSubmit)}>
-//           <Controller
-//             control={control}
-//             name={'newPassword'}
-//             render={({ field }) => (
-//               <Input
-//                 {...field}
-//                 error={errors.newPassword?.message}
-//                 onChange={field.onChange}
-//                 placeholder={'New password'}
-//                 variant={'password'}
-//               />
-//             )}
-//           />
-//           <div className={s.space}></div>
-//           <Controller
-//             control={control}
-//             name={'confirmPassword'}
-//             render={({ field }) => (
-//               <Input
-//                 {...field}
-//                 error={errors.confirmPassword?.message}
-//                 onChange={field.onChange}
-//                 placeholder={'Password confirmation'}
-//                 variant={'password'}
-//               />
-//             )}
-//           />
-
-//           <Typography className={s.helptext} variant={'body1'}>
-//             Your password must be between 6 and 20 characters
-//           </Typography>
-//           <div className={s.spacer}></div>
-//           <Button as={'button'} className={s.btn} fullWidth variant={'primary'}>
-//             Create new password
-//           </Button>
-//         </form>
-//       </Card>
-//     </div>
-//   )
-// }
+  return (
+    <div className={s.wrapper}>
+      <Card className={s.card}>
+        <h3 className={s.textWrapper}>Create New Password</h3>
+        <form onSubmit={handleSubmit(onSubmit)}>
+          <div className={s.form}>
+            <div>
+              <Controller
+                control={control}
+                name="newPassword"
+                render={({ field }) => (
+                  <FormInput
+                    {...field}
+                    control={control}
+                    error={errors.newPassword?.message}
+                    placeholder="New password"
+                    variant="password"
+                  />
+                )}
+              />
+            </div>
+            <div>
+              <Controller
+                control={control}
+                name="confirmPassword"
+                render={({ field }) => (
+                  <FormInput
+                    {...field}
+                    control={control}
+                    error={errors.confirmPassword?.message}
+                    placeholder="Password confirmation"
+                    variant="password"
+                  />
+                )}
+              />
+            </div>
+          </div>
+          <div className={s.infoWrapper}>
+            <div>
+              <Typography className={s.infoText} variant="body1">
+                Your password must be between 6 and 20 characters
+              </Typography>
+            </div>
+            <div className={s.btnWrapper}>
+              <Button as="button" className={s.btn} fullWidth variant="primary">
+                Create new password
+              </Button>
+            </div>
+          </div>
+        </form>
+      </Card>
+    </div>
+  )
+}

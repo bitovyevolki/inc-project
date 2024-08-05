78a2c861de1c97ef04cda7a65e1937c6180a021e
Dima <dmitrijs.visnakovs@inbox.lv>
2024-08-05 21:56:16 +0300

    add pages

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -4,11 +4,14 @@ export default function Home() {
   return (
     <div>
       <nav className={'navigation'}>
-        <Link href={'/forgot-password'}>Forgot password</Link>
+        <Link href={'/personal-info'}>Person info</Link>
         <Link href={'/signin'}>Sign In</Link>
         <Link href={'/signup'}>Sign Up</Link>
         <Link href={'/create-new-password'}>Create password</Link>
-        <Link href={'/personal-info'}>Person info</Link>
+        <Link href={'/forgot-password'}>Forgot password</Link>
+        <Link href={'/verification-link'}>Verification Link</Link>
+        <Link href={'/terms'}>Terms</Link>
+        <Link href={'/privacy-policy'}>Privacy Policy</Link>
       </nav>
     </div>
   )

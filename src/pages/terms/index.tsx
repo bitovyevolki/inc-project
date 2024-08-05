4d50ca468cac0d337f0c45758642cf70fdb3001e
Dima <dmitrijs.visnakovs@inbox.lv>
2024-08-05 22:12:06 +0300

    changes

--- src/pages/terms/index.tsx
+++ src/pages/terms/index.tsx
@@ -5,7 +5,11 @@ export default function SignIn() {
   return (
     <div>
       <Link href={'/'}>back</Link>
-      <TermsOfService text={'text'} />
+      <TermsOfService
+        text={
+          'Lorem ipsum dolor sit amet, consectetur adipisicing elit. Libero, consequatur ipsum voluptatibus cumque nam qui alias deserunt earum veniam praesentium? Natus enim officia et aspernatur unde beatae facilis assumenda sunt'
+        }
+      />
     </div>
   )
 }

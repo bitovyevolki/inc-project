ec89b61cc2e54923bc2337312ee56a5c829423a7
BukreevSergey <bukreevwork2@gmail.com>
2024-08-07 11:50:30 +0300

    add base service, auth, profile services. Adding Auto-Refreshing Tokens

---
+++ src/features/auth/service/auth.service.ts
@@ -0,0 +1,9 @@
+import { inctagramService } from '@/src/shared/model/inctagram.service'
+
+export const AuthService = inctagramService.injectEndpoints({
+  endpoints: builder => {
+    return {
+      /// ADD Your Endpoints
+    }
+  },
+})
--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,10 +1,13 @@
 import { ReactElement, ReactNode } from 'react'
+import { Provider } from 'react-redux'
 
 import { Header } from '@bitovyevolki/ui-kit-int'
 import { NextPage } from 'next'
 import { AppProps } from 'next/app'
 
 import '../styles/globals.scss'
+
+import { wrapper } from './../shared/model/store'
 
 export type NextPageWithLayout<P = {}, IP = P> = {
   getLayout?: (page: ReactElement) => ReactNode
@@ -14,13 +17,16 @@ type AppPropsWithLayout = {
   Component: NextPageWithLayout
 } & AppProps
 
-export default function MyApp({ Component, pageProps }: AppPropsWithLayout) {
+export default function MyApp({ Component, ...rest }: AppPropsWithLayout) {
   const getLayout = Component.getLayout ?? (page => page)
+  const { props, store } = wrapper.useWrappedStore(rest)
 
   return (
-    <section>
-      <Header isAuth onLanguageChange={() => {}} title={'Inctagram'} />
-      <main>{getLayout(<Component {...pageProps} />)}</main>
-    </section>
+    <Provider store={store}>
+      <section>
+        <Header isAuth onLanguageChange={() => {}} title={'Inctagram'} />
+        <main>{getLayout(<Component {...props.pageProps} />)}</main>
+      </section>
+    </Provider>
   )
 }

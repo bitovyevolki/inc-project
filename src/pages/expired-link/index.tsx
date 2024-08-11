9b120b384687f7488e20b876856312adc3ef9da9
Dima <dmitrijs.visnakovs@inbox.lv>
2024-08-11 17:07:23 +0300

    fix

--- src/pages/expired-link/index.tsx
+++ src/pages/expired-link/index.tsx
@@ -2,10 +2,11 @@ import ExpiredLink from '@/src/features/auth/expiredLink/ui/ExpiredLink'
 import Link from 'next/link'
 
 export default function ExpiredLinkPage() {
+  const email = 'test@test.com'
   return (
     <div>
       <Link href={'/'}>back</Link>
-      <ExpiredLink />
+      <ExpiredLink email={email} />
     </div>
   )
 }

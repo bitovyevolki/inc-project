d5a5e2776d7c0024308416d6dd71e697ccf3d63e
artFetisov <fetishfestoff@mail.ru>
2024-08-16 17:29:39 +0300

    sidebar feat" add translate

--- src/shared/ui/Sidebar/Sidebar.tsx
+++ src/shared/ui/Sidebar/Sidebar.tsx
@@ -1,11 +1,41 @@
+import React from 'react'
+
 import { Typography } from '@bitovyevolki/ui-kit-int'
 import Link from 'next/link'
+import { useTranslations } from 'next-intl'
 
 import s from './Sidebar.module.scss'
 
-import { sidebarLinks } from './links'
+import { RouterPaths } from '../../config/router.paths'
+import {
+  CreateIcon,
+  FavoritesIcon,
+  HomeIcon,
+  MessengerIcon,
+  MyProfileIcon,
+  SearchIcon,
+  StatisticsIcon,
+} from './Icons'
+
+interface ILink {
+  path: string
+  svg: () => React.JSX.Element
+  title: string
+}
 
 export const Sidebar = () => {
+  const t = useTranslations('Sidebar')
+
+  const sidebarLinks: ILink[] = [
+    { path: RouterPaths.HOME, svg: HomeIcon, title: t('home') },
+    { path: RouterPaths.HOME, svg: CreateIcon, title: t('create') },
+    { path: RouterPaths.HOME, svg: MyProfileIcon, title: t('my-profile') },
+    { path: RouterPaths.HOME, svg: MessengerIcon, title: t('messenger') },
+    { path: RouterPaths.HOME, svg: SearchIcon, title: t('search') },
+    { path: RouterPaths.HOME, svg: StatisticsIcon, title: t('statistics') },
+    { path: RouterPaths.HOME, svg: FavoritesIcon, title: t('favorites') },
+  ]
+
   return (
     <nav className={s.sidebar}>
       {sidebarLinks.map(l => (

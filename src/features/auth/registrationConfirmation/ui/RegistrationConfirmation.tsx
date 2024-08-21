fde268de99b9cab8a0c32ff0e4f3cb8ea62ee2e5
VladimirS <v.a.samarin@yandex.ru>
2024-08-21 08:02:15 +0300

    fix confirmation

--- src/features/auth/registrationConfirmation/ui/RegistrationConfirmation.tsx
+++ src/features/auth/registrationConfirmation/ui/RegistrationConfirmation.tsx
@@ -40,14 +40,3 @@ export function RegistrationConfirmation() {
     )
   }
 }
-
-export const getServerSideProps: GetServerSideProps = async context => {
-  const locale = context.req.cookies['next-language'] || 'en'
-  const messages = (await import(`../../locales/${locale}.json`)).default
-
-  return {
-    props: {
-      messages,
-    },
-  }
-}

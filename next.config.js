cda5622bcde29e9b3ca8cceff2b7fb53832041eb
VladimirS <v.a.samarin@yandex.ru>
2024-10-13 11:00:32 +0300

    test

--- next.config.js
+++ next.config.js
@@ -1,4 +1,7 @@
 const createNextIntlPlugin = require('next-intl/plugin')
+const { container } = require('webpack')
+const ModuleFederationPlugin = container.ModuleFederationPlugin
+const { dependencies: deps, name } = require('./package.json')
 
 const withNextIntl = createNextIntlPlugin()
 
@@ -6,6 +9,25 @@ const withNextIntl = createNextIntlPlugin()
 const nextConfig = {
   images: {
     domains: ['staging-it-incubator.s3.eu-central-1.amazonaws.com'],
+  },
+  webpack: (config, { isServer }) => {
+    config.plugins.push(
+      new ModuleFederationPlugin({
+        exposes: {},
+        filename: 'static/chunks/remoteEntry.js',
+        name,
+        remotes: {
+          messenger: `messenger@http://localhost:3001/remoteEntry.js`,
+        },
+        shared: {
+          ...deps,
+          react: { eager: true, requiredVersion: deps.react, singleton: true },
+          'react-dom': { eager: true, requiredVersion: deps['react-dom'], singleton: true },
+        },
+      })
+    )
+
+    return config
   },
 }
 

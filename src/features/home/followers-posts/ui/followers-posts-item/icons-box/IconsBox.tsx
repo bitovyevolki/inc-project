7681e1f5e1eaae20031ddd6cf04627a2eaaa5ea0
BukreevSergey <bukreevwork2@gmail.com>
2024-10-22 12:21:34 +0300

    feat: add logic for share post from home page

--- src/features/home/followers-posts/ui/followers-posts-item/icons-box/IconsBox.tsx
+++ src/features/home/followers-posts/ui/followers-posts-item/icons-box/IconsBox.tsx
@@ -1,4 +1,8 @@
+import React, { useState } from 'react'
+
 import { useUpdatePostLikeMutation } from '@/src/features/post/model/posts.service'
+import { SharePost } from '@/src/features/post/ui/sharePost/sharePost'
+import { ViewPostModal } from '@/src/features/post/ui/viewPostModal'
 import { BookmarkIcon, LikeIcon, MessageIcon, PaperPlaneIcon } from '@/src/shared/assets/icons'
 import { RouterPaths } from '@/src/shared/config/router.paths'
 import clsx from 'clsx'
@@ -7,14 +11,15 @@ import { useRouter } from 'next/router'
 import s from './IconsBox.module.scss'
 
 interface IProps {
-  copyUrl: () => void
   isLiked?: boolean
   ownerId: number
   postId: number
+  postUrl: string
 }
 
-export const IconsBox = ({ copyUrl, isLiked, ownerId, postId }: IProps) => {
+export const IconsBox = ({ isLiked, ownerId, postId, postUrl }: IProps) => {
   const [updatePostLikeStatus] = useUpdatePostLikeMutation()
+  const [shareMode, setShareMode] = useState<boolean>(false)
 
   const router = useRouter()
 
@@ -24,6 +29,13 @@ export const IconsBox = ({ copyUrl, isLiked, ownerId, postId }: IProps) => {
 
   const addLikeHandler = () => {
     updatePostLikeStatus({ likeStatus: isLiked ? 'DISLIKE' : 'LIKE', postId })
+  }
+  const openShareModal = () => {
+    setShareMode(true)
+  }
+
+  const closeShareModal = () => {
+    setShareMode(false)
   }
 
   const likeColor = isLiked ? 'red' : 'white'
@@ -38,8 +50,13 @@ export const IconsBox = ({ copyUrl, isLiked, ownerId, postId }: IProps) => {
         width={24}
       />
       <MessageIcon className={s.icon} onClick={toMessengerHandler} />
-      <PaperPlaneIcon className={s.icon} onClick={copyUrl} />
+      <PaperPlaneIcon className={s.icon} onClick={openShareModal} />
       <BookmarkIcon className={clsx(s.lastIcon, s.icon)} />
+      {shareMode && (
+        <ViewPostModal isOpen={shareMode} onOpenChange={closeShareModal}>
+          <SharePost onClose={closeShareModal} postUrl={postUrl} />
+        </ViewPostModal>
+      )}
     </div>
   )
 }
